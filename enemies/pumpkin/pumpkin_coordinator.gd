cc80797e63b47beaab6dc92be9d3d0d611a894ca
Joshua Lutes <joshua@lutes.me>
2019-11-12 22:30:10 -0800

    Place Timer in Pumpkin Coordinator Scene

--- enemies/pumpkin/pumpkin_coordinator.gd
+++ enemies/pumpkin/pumpkin_coordinator.gd
@@ -13,16 +13,15 @@ var pumpkins = []
 var active_pumpkins = []
 var room: network.Room
 
-var timer = Timer.new()
-
 func _init():
 	room = network.get_room(position)
 	room_to_coordinator[room] = self
+
+func _ready():
 	room.connect("first_player_entered", self, "start_timer")
 	room.connect("last_player_exited", self, "stop_timer")
-	timer.connect("timeout", self, "activate_pumpkin")
-	timer.set_wait_time(ACTIVATE_TIME)
-	add_child(timer)
+	$Timer.connect("timeout", self, "activate_pumpkin")
+	$Timer.set_wait_time(ACTIVATE_TIME)
 	call_deferred("start_timer")
 
 func register_pumpkin(pumpkin):
@@ -30,15 +29,15 @@ func register_pumpkin(pumpkin):
 	start_timer()
 
 func start_timer():
-	if timer.is_stopped() and pumpkins.size() > 0 and !room.players.empty():
-		timer.start()
+	if $Timer.is_stopped() and pumpkins.size() > 0 and !room.players.empty():
+		$Timer.start()
 
 func stop_timer():
-	timer.stop()
+	$Timer.stop()
 
 func activate_pumpkin():
 	var pumpkin = pumpkins.pop_front()
 	pumpkin.activate()
 	active_pumpkins.push_back(pumpkin)
 	if pumpkins.size() == 0:
-		timer.stop()
+		$Timer.stop()
